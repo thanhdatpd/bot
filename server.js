740de9266c9c5a734dcbcbc97ba8bdb539164402
ThanhdatPD <thanhdatppro@gmail.com>
2017-11-30 23:57:21 +0700

    2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-//  OpenShift sample Node application
+/*//  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-/* morgan  = require('morgan');
+ morgan  = require('morgan');
     
 Object.assign=require('object-assign')
 
